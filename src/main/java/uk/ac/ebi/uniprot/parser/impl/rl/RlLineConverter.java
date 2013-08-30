8387784fabeec914f7566486a70d103fa9d4529a
Jie Luo <jluo@ebi.ac.uk>
2013-08-30 16:04:21 +0100

    update RlLineConverter

--- src/main/java/uk/ac/ebi/uniprot/parser/impl/rl/RlLineConverter.java
+++ src/main/java/uk/ac/ebi/uniprot/parser/impl/rl/RlLineConverter.java
@@ -1,7 +1,11 @@
 package uk.ac.ebi.uniprot.parser.impl.rl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.ac.ebi.kraken.interfaces.uniprot.citationsNew.Book;
 import uk.ac.ebi.kraken.interfaces.uniprot.citationsNew.Citation;
+import uk.ac.ebi.kraken.interfaces.uniprot.citationsNew.Editor;
 import uk.ac.ebi.kraken.interfaces.uniprot.citationsNew.JournalArticle;
 import uk.ac.ebi.kraken.model.factories.DefaultCitationNewFactory;
 import uk.ac.ebi.uniprot.parser.Converter;
@@ -37,7 +41,19 @@ public class RlLineConverter implements Converter<RlLineObject, Citation> {
 		 citation.setLastPage(factory.buildPage(""+b.page_end));
 		 citation.setPublicationDate(factory.buildPublicationDate(""+b.year));
 		 citation.setVolume(factory.buildVolume("" + b.volume));
-		
+		 citation.setBookName(factory.buildBookName(b.title));
+		 List<Editor> editors = new ArrayList<>();
+		 for(String val:b.editors){
+			 editors.add(factory.buildEditor(val));
+		 }
+		 citation.setEditors(editors);
+		 if(b.press !=null &&( !b.press.isEmpty())){
+			 citation.setPublisher(factory.buildPublisher(b.press));
+		 }
+		 if((b.place !=null) &&(!b.place.isEmpty())){
+			 citation.setCity(factory.buildCity(b.place));
+		 }
 		return citation;
 	}
+	
 }
