2f396a16fd67ef51c9ba429b84275f1cc6f4048a
Wudong Liu <wudong.liu@gmail.com>
2013-10-02 17:36:19 +0100

    add test abilitity to skip until see an accession.

--- src/main/java/uk/ac/ebi/uniprot/parser/tool/FFParser.java
+++ src/main/java/uk/ac/ebi/uniprot/parser/tool/FFParser.java
@@ -15,23 +15,53 @@ public class FFParser {
 
 	public static void main(String[] args) throws IOException {
 		FFParser ffParser = new FFParser();
+		if (args.length==2){
+			ffParser.setStartParingString(args[1]);
+		}
 		ffParser.parse(args[0]);
 	}
 
+	private String startParingString;
+
+	public String getStartParingString() {
+		return startParingString;
+	}
+
+	public void setStartParingString(String startParingString) {
+		this.startParingString = startParingString;
+	}
+
 	public void parse(String in) throws IOException {
+		boolean startParsing=true;
+		if (this.startParingString!=null)startParsing=false;
+
 		EntryBufferedReader reader = new EntryBufferedReader(in);
 		String entry = reader.next();
+
+		if (this.startParingString!=null){
+			startParsing = entry.startsWith(this.startParingString);
+		}
 
 		int count = 0;
 		long time = 0;
 		try {
 			while (entry != null) {
-				count++;
-				long start = System.currentTimeMillis();
-				EntryObject entryObject = parseEntry(entry);
-				System.out.println(entryObject.id.entryName);
+
+				if (startParsing){
+					count++;
+					long start = System.currentTimeMillis();
+					EntryObject entryObject = parseEntry(entry);
+					System.out.println(entryObject.id.entryName);
+					time += System.currentTimeMillis() - start;
+				}
+
 				entry = reader.next();
-				time += System.currentTimeMillis() - start;
+
+				if (!startParsing&&this.startParingString!=null){
+					startParsing = entry.startsWith(this.startParingString);
+				}
+
+
 			}
 		} finally {
 			System.out.println(String.format("parsing %s entries total time used %s", count, time));
