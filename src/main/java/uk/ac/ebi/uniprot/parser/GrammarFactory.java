4d7684f1ff3df49188ee9204da67a441672b673a
Wudong Liu <wudong.liu@gmail.com>
2013-10-23 12:31:17 +0100

    remove reflection.

--- src/main/java/uk/ac/ebi/uniprot/parser/GrammarFactory.java
+++ src/main/java/uk/ac/ebi/uniprot/parser/GrammarFactory.java
@@ -1,9 +1,10 @@
 package uk.ac.ebi.uniprot.parser;
 
-import com.google.common.base.Throwables;
-import org.antlr.v4.runtime.*;
-
-import java.lang.reflect.Constructor;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.Parser;
+import uk.ac.ebi.uniprot.parser.antlr.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,7 +15,7 @@ import java.lang.reflect.Constructor;
  */
 public interface GrammarFactory<L extends Lexer, P extends Parser> {
 
-	public static final String packageName = "uk.ac.ebi.uniprot.parser.antlr";
+	//public static final String packageName = "uk.ac.ebi.uniprot.parser.antlr";
 
 	public static enum GrammarFactoryEnum {
 		Uniprot, Ac, Id, Dt, Kw, Dr, Sq, Gn, Pe, Os, Og, Rn, Rt, Rp, Ra, Rg, Rc, Rx, De, Rl, Ft, Oc, Ox, Oh, Cc, Ss;
@@ -24,39 +25,84 @@ public interface GrammarFactory<L extends Lexer, P extends Parser> {
 		private GrammarFactory createFactory() {
 			final String name = this.name();
 
-			final String lexerName = packageName + "." + name + (name.equals("Uniprot")?"":"Line") + "Lexer";
-			final String parserName = packageName + "." + name + (name.equals("Uniprot")?"":"Line") +"Parser";
+			//final String lexerName = packageName + "." + name + (name.equals("Uniprot")?"":"Line") + "Lexer";
+			//final String parserName = packageName + "." + name + (name.equals("Uniprot")?"":"Line") +"Parser";
 
 			return new GrammarFactory() {
 				@Override
 				public Lexer createLexer(CharStream in) {
-					try {
-						Class<? extends Lexer> aClass = (Class<? extends Lexer>) Class.forName(lexerName);
-						Constructor<? extends Lexer> constructor = aClass.getConstructor(CharStream.class);
-						Lexer lexer = constructor.newInstance(in);
-						return lexer;
-					} catch (Exception e) {
-						throw Throwables.propagate(e);
-					}
+                    switch (GrammarFactoryEnum.this) {
+                        case Uniprot:return new UniprotLexer(in);
+                        case Ac:return new AcLineLexer(in);
+                        case Id:return new IdLineLexer(in);
+                        case Dt:return new DtLineLexer(in);
+                        case Kw:return new KwLineLexer(in);
+                        case Dr:return new DrLineLexer(in);
+                        case Sq:return new SqLineLexer(in);
+                        case Gn:return new GnLineLexer(in);
+                        case Pe:return new PeLineLexer(in);
+                        case Os:return new OsLineLexer(in);
+                        case Og:return new OgLineLexer(in);
+                        case Rn:return new RnLineLexer(in);
+                        case Rt:return new RtLineLexer(in);
+                        case Rp:return new RpLineLexer(in);
+                        case Ra:return new RaLineLexer(in);
+                        case Rg:return new RgLineLexer(in);
+                        case Rc:return new RcLineLexer(in);
+                        case Rx:return new RxLineLexer(in);
+                        case De:return new DeLineLexer(in);
+                        case Rl:return new RlLineLexer(in);
+                        case Ft:return new FtLineLexer(in);
+                        case Oc:return new OcLineLexer(in);
+                        case Ox:return new OxLineLexer(in);
+                        case Oh:return new OhLineLexer(in);
+                        case Cc:return new CcLineLexer(in);
+                        case Ss:return new SsLineLexer(in);
+
+                        default: throw new RuntimeException ("Lexer is not defined for: "+ GrammarFactoryEnum.this);
+                    }
+
 				}
 
 				@Override
 				public Parser createParser(CommonTokenStream tokens) {
-					try {
-						Class<? extends Parser> aClass = (Class<? extends Parser>) Class.forName(parserName);
-						Constructor<? extends Parser> constructor = aClass.getConstructor(TokenStream.class);
-						Parser lexer = constructor.newInstance(tokens);
-						return lexer;
-					} catch (Exception e) {
-						throw Throwables.propagate(e);
-					}
+                    switch (GrammarFactoryEnum.this) {
+                        case Uniprot:return new UniprotParser(tokens);
+                        case Ac:return new AcLineParser(tokens);
+                        case Id:return new IdLineParser(tokens);
+                        case Dt:return new DtLineParser(tokens);
+                        case Kw:return new KwLineParser(tokens);
+                        case Dr:return new DrLineParser(tokens);
+                        case Sq:return new SqLineParser(tokens);
+                        case Gn:return new GnLineParser(tokens);
+                        case Pe:return new PeLineParser(tokens);
+                        case Os:return new OsLineParser(tokens);
+                        case Og:return new OgLineParser(tokens);
+                        case Rn:return new RnLineParser(tokens);
+                        case Rt:return new RtLineParser(tokens);
+                        case Rp:return new RpLineParser(tokens);
+                        case Ra:return new RaLineParser(tokens);
+                        case Rg:return new RgLineParser(tokens);
+                        case Rc:return new RcLineParser(tokens);
+                        case Rx:return new RxLineParser(tokens);
+                        case De:return new DeLineParser(tokens);
+                        case Rl:return new RlLineParser(tokens);
+                        case Ft:return new FtLineParser(tokens);
+                        case Oc:return new OcLineParser(tokens);
+                        case Ox:return new OxLineParser(tokens);
+                        case Oh:return new OhLineParser(tokens);
+                        case Cc:return new CcLineParser(tokens);
+                        case Ss:return new SsLineParser(tokens);
+
+                        default: throw new RuntimeException ("Parser is not defined for: "+ GrammarFactoryEnum.this);
+                    }
 				}
 
-				@Override
-				public String getTopRuleName() {
-					if (name.equals("Uniprot")) return "entry";
-					return (name + "_" + name).toLowerCase();
-				}
+//				@Override
+//				public String getTopRuleName() {
+//					if (name.equals("Uniprot")) return "entry";
+//					return (name + "_" + name).toLowerCase();
+//				}
 			};
 		}
 
@@ -72,7 +118,7 @@ public interface GrammarFactory<L extends Lexer, P extends Parser> {
 
 	public P createParser(CommonTokenStream tokens);
 
-	public String getTopRuleName();
+	//public String getTopRuleName();
 
 
 }
